e359bd58d32b3353c9c64d6b4417a506547708a6
Anthony Romero-Jorge <AnthonyRomero@MacBook-Pro.local>
2022-04-13 14:48:25 -0400

    added the VERY EASY and EASY coding challenges

---
+++ guidedInquiry.js
@@ -0,0 +1,18 @@
+//VERY EASY
+const num1 = 56;
+const num2 = 28;
+console.log(num1 - num2);
+
+//EASY
+const broName = "Oscar";
+const sisName = "Yoselin";
+const nameDiff = sisName.length - broName.length;
+console.log(`The name Yoselin is longer than Oscar by ${nameDiff} characters.`);
+
+//MEDIUM
+
+//HARD
+
+//VERY HARD
+
+
